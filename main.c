d13ea0ae86a6f08be6679e96402fa27e895740d8
2shlomi9 <2shlomiariel9@gmail.com>
2024-02-06 12:57:38 +0200

    remove files

--- main.c
+++ main.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-int main() {
-    // Redirecting standard input to read from "input1.txt"
-    freopen("../inputs/input1.txt", "r", stdin);
-    // Redirecting standard output to write to "tmp_out.txt"
-    freopen("tmp_out.txt", "w", stdout);
 
+int main()
+{
     char chose;
     int stop = 1;
     do {
-        scanf("%c", &chose);
-        if (chose == 'A') {
+        scanf("%c" , &chose );
+        if(chose == 'A'){
             scanMatrix();
-        } else if (chose == 'B') {
+        }else if(chose == 'B') {
             int p1, p2;
-            scanf("%d%d", &p1, &p2);
+            scanf("%d%d", &p1 ,&p2);
             isExistsPath(p1, p2);
-        } else if (chose == 'C') {
+        }else if (chose == 'C') {
             int V1, V2;
-            scanf("%d%d", &V1, &V2);
+            scanf("%d%d", &V1 ,&V2);
             shortestPath(V1, V2);
-        } else if (chose == 'D') {
+        }else if (chose == 'D') {
             stop = 0;
         }
-    } while (stop != 0);
-
+    }while(stop != 0);
     return 0;
 }
